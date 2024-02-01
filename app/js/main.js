f3dc59e825995bc8775964f1374132a9742baa44
Enzhy58 <ang.borisova2015@yandex.ru>
2024-02-01 13:28:28 +0300

    added reviews

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.reviews__list').slick({
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/prenatalFitness/reviews/prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/prenatalFitness/reviews/next.svg" alt=""></button>'
+    });
+});
